6f780fe789535aed987d40d5d2ad47f1e7c16f31
AdrianHsu <adrianhsu1995@gmail.com>
2017-06-08 20:21:40 +0800

    menu inflate

--- BioExp/app/src/main/java/dots/adrianhsu/bioexp/MainActivity.java
+++ BioExp/app/src/main/java/dots/adrianhsu/bioexp/MainActivity.java
@@ -3,6 +3,8 @@ package dots.adrianhsu.bioexp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -57,5 +59,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.analysis:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- BioExp/app/src/main/java/dots/adrianhsu/bioexp/ShowChartActivity.java
+++ BioExp/app/src/main/java/dots/adrianhsu/bioexp/ShowChartActivity.java
@@ -18,6 +18,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
